494147aab0a5d33c9e6c54d19b667a135b6a11f9
Adam Dick <adam.c.dick@gmail.com>
2019-05-09 20:05:57 -0400

    updated classification models

--- Source/classifiers.py
+++ Source/classifiers.py
@@ -45,18 +45,11 @@ def grid_search(grid_searches, X_train, X_test, y_train, y_test, columns):
         gs['y_hat_train'] = gs['Classifier'].predict(X_train)
         gs['y_hat_test']  = gs['Classifier'].predict(X_test)
         
-        if gs['Model'] == 'Decision Tree':
-            features = list(zip(columns, gs['Classifier'].best_estimator_.feature_importances_))
-        elif gs['Model'] == 'Random Forest':
-            features = list(zip(columns, gs['Classifier'].best_estimator_.feature_importances_)) 
-        else:
-            features = list(zip(columns, gs['Classifier'].best_estimator_.coef_[0]))
-        
         # append all training metrics
         metrics.append({'Model':            gs['Model'],
                         'Split':            'Train',
                         'Best Parameters':  gs['Classifier'].best_params_,
-                        'Features':         features,
+                        'Best Estimator':   gs['Classifier'].best_estimator_,
                         'Accuracy':         accuracy_score(  y_train, gs['y_hat_train']),
                         'Precision':        precision_score( y_train, gs['y_hat_train'], average='weighted'),
                         'Recall':           recall_score(    y_train, gs['y_hat_train'], average='weighted'),
@@ -67,7 +60,7 @@ def grid_search(grid_searches, X_train, X_test, y_train, y_test, columns):
         metrics.append({'Model':            gs['Model'],
                         'Split':            'Test',
                         'Best Parameters':  gs['Classifier'].best_params_,
-                        'Features':         features,
+                        'Best Estimator':   gs['Classifier'].best_estimator_,
                         'Accuracy':         accuracy_score(  y_test, gs['y_hat_test']),
                         'Precision':        precision_score( y_test, gs['y_hat_test'], average='weighted'),
                         'Recall':           recall_score(    y_test, gs['y_hat_test'], average='weighted'),
