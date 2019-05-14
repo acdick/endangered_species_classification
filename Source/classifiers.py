15717dde861ff151762c374129c4271313f67c72
Adam Dick <adam.c.dick@gmail.com>
2019-05-14 18:02:03 -0400

    updated grid search function

--- Source/classifiers.py
+++ Source/classifiers.py
@@ -1,42 +1,54 @@
+from sklearn.model_selection import GridSearchCV
+
+from sklearn.dummy        import DummyClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes  import GaussianNB
+from sklearn.neighbors    import KNeighborsClassifier
+from sklearn.tree         import DecisionTreeClassifier
+from sklearn.ensemble     import RandomForestClassifier
+from sklearn.ensemble     import AdaBoostClassifier
+
 from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import f1_score
 from sklearn.metrics import confusion_matrix
 
-def fit_predict_metrics(classifiers, X_train, X_test, y_train, y_test):
+import pandas as pd
+
+def grid_search_dummy_classifier(parameters, scoring):
     
-    metrics = []
+    classifier = {'Classifier': 'Dummy',
+                  'Grid Search': GridSearchCV(
+                      DummyClassifier(),
+                      parameters,
+                      cv=5,
+                      scoring=scoring,
+                      refit='f1_weighted',
+                      return_train_score=False,
+                      verbose=1)}
+    
+    return classifier
+
+def fit_predict_measure(data_name, X_train, X_test, y_train, y_test, y_labels, classifiers):
+    
+    all_models = pd.DataFrame()
     
     for classifier in classifiers:
         
-        # fit all classifiers
-        classifier['Classifier'].fit(X_train, y_train)
+        # fit training set
+        classifier['Grid Search'].fit(X_train, y_train)
         
-        # predict and append all training metrics
-        classifier['y_hat_train'] = classifier['Classifier'].predict(X_train)
+        # cross-validated training metrics
+        train = pd.DataFrame(classifier['Grid Search'].cv_results_)
+        train = train[['params', 'mean_test_accuracy', 'mean_test_f1_weighted']]
+        train['Data']       = data_name
+        train['Classifier'] = classifier['Classifier']
+        train['Split']      = 'Train'
         
-        metrics.append({'Model':            classifier['Model'],
-                        'Split':            'Train',
-                        'Best Parameters':  classifier['Classifier'].best_params_,
-                        'Best Estimator':   classifier['Classifier'].best_estimator_,
-                        'Accuracy':         accuracy_score(  y_train, classifier['y_hat_train']),
-                        'Precision':        precision_score( y_train, classifier['y_hat_train'], average='weighted'),
-                        'Recall':           recall_score(    y_train, classifier['y_hat_train'], average='weighted'),
-                        'F1 Score':         f1_score(        y_train, classifier['y_hat_train'], average='weighted'),
-                        'Confusion Matrix': confusion_matrix(y_train, classifier['y_hat_train'])})
+        # hold-out test performance
+        y_hat_test = classifier['Grid Search'].predict(X_test)
+        accuracy_score(y_test, y_hat_test)
+        test['Confusion Matrix'] = confusion_matrix(y_test, y_hat_test)
         
-        # predict and append all testing metrics
-        classifier['y_hat_test'] = classifier['Classifier'].predict(X_test)
-        
-        metrics.append({'Model':            classifier['Model'],
-                        'Split':            'Test',
-                        'Best Parameters':  classifier['Classifier'].best_params_,
-                        'Best Estimator':   classifier['Classifier'].best_estimator_,
-                        'Accuracy':         accuracy_score(  y_test, classifier['y_hat_test']),
-                        'Precision':        precision_score( y_test, classifier['y_hat_test'], average='weighted'),
-                        'Recall':           recall_score(    y_test, classifier['y_hat_test'], average='weighted'),
-                        'F1 Score':         f1_score(        y_test, classifier['y_hat_test'], average='weighted'),
-                        'Confusion Matrix': confusion_matrix(y_test, classifier['y_hat_test'])})
-    
-    return classifiers, metrics
+    return train
